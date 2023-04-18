3961cc9225fbfb70574aa9b6fd4c1acc7222c21b
kitety <aizaizuori@foxmail.com>
2023-04-18 12:10:52 +0800

    feat: 唤起

--- src/main/index.js
+++ src/main/index.js
@@ -1,13 +1,45 @@
 const { app, BrowserWindow } = require('electron')
-const path = require('path')
+const path = require('path');
+const { URLSearchParams } = require('url');
 
-let win;
-app.whenReady().then(() => {
-    win = new BrowserWindow({
-        width: 800,
-        height: 600,
-        titleBarStyle: 'hiddenInset',
-    })
-    win.loadFile(path.join(__dirname, '../renderer/index.html'))
-    win.webContents.openDevTools()
+const protocol = 'electron-desktop'
+const scheme = `${protocol}://`
+app.setAsDefaultProtocolClient(protocol)
+
+let urlParam = {};
+// electron-desktop://a=1
+app.on('open-url', (event, url) => {
+    console.log('url: ', url);
+
+    const urlParams = new URLSearchParams(url.slice(scheme.length))
+    urlParam = Object.fromEntries(urlParams.entries())
+    console.log('urlParam: ', urlParam);
+
 })
+
+let mainWindow;
+const gotTheLock = app.requestSingleInstanceLock()
+if (!gotTheLock) {
+    app.quit()
+} else {
+    app.on('second-instance', (event, argv, workingDirectory) => {
+        // Mac 平台只需要展示窗口即可
+        mainWindow.restore()
+        mainWindow.show()
+
+        // Windows 平台上需要判断新的实例是否被 scheme 唤起
+        const url = argv.find(v => v.startsWith(scheme))
+        if (url) { // 如果发现 electron-desktop:// 前缀，说明是通过 scheme 唤起
+            console.log(url)
+        }
+    })
+}
+
+app.whenReady().then(() => {
+    createWindow()
+})
+
+function createWindow() {
+    mainWindow = new BrowserWindow({ width: 800, height: 600 })
+    mainWindow.loadURL('https://www.juejin.cn')
+}
